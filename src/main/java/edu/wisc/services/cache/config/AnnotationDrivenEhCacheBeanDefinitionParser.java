9e9206ec6c4d6688f968b6d5823f50bba23e2d93
npblair <npblair@e9fffd1a-4083-750a-f54f-a748c1f33b81>
2010-03-18 19:52:57 +0000

    new annotation driven BeanDefinitionParser and NamespaceHandler implementations
    git-svn-id: http://ehcache-spring-annotations.googlecode.com/svn/spring-ehcache/trunk@17 e9fffd1a-4083-750a-f54f-a748c1f33b81

---
+++ src/main/java/edu/wisc/services/cache/config/AnnotationDrivenEhCacheBeanDefinitionParser.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright 2010 The Board of Regents of the University of Wisconsin System.
+ */
+package edu.wisc.services.cache.config;
+
+import org.springframework.aop.config.AopNamespaceUtils;
+import org.springframework.aop.support.DefaultBeanFactoryPointcutAdvisor;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.RuntimeBeanReference;
+import org.springframework.beans.factory.support.RootBeanDefinition;
+import org.springframework.beans.factory.xml.BeanDefinitionParser;
+import org.springframework.beans.factory.xml.ParserContext;
+import org.w3c.dom.Element;
+
+import edu.wisc.services.cache.annotations.CacheStaticMethodMatcherPointcut;
+import edu.wisc.services.cache.impl.CacheableAttributeSourceImpl;
+import edu.wisc.services.cache.interceptor.caching.CachingInterceptor;
+
+/**
+ * {@link BeanDefinitionParser} t
+ * @author Nicholas Blair, nblair@doit.wisc.edu
+ * @version $Id$
+ */
+public class AnnotationDrivenEhCacheBeanDefinitionParser implements BeanDefinitionParser {
+
+	public static final String EHCACHE_ADVISOR_BEAN_NAME ="edu.wisc.services.cache.config.internalEhCacheAdvisor";
+	/* (non-Javadoc)
+	 * @see org.springframework.beans.factory.xml.BeanDefinitionParser#parse(org.w3c.dom.Element, org.springframework.beans.factory.xml.ParserContext)
+	 */
+	@Override
+	public BeanDefinition parse(Element element, ParserContext parserContext) {
+		AopNamespaceUtils.registerAutoProxyCreatorIfNecessary(parserContext, element);
+		if (!parserContext.getRegistry().containsBeanDefinition(EHCACHE_ADVISOR_BEAN_NAME)) {
+			Object elementSource = parserContext.extractSource(element);
+			
+			RootBeanDefinition cacheableAttributeSource = new RootBeanDefinition(CacheableAttributeSourceImpl.class);
+			cacheableAttributeSource.setSource(elementSource);
+			cacheableAttributeSource.setRole(BeanDefinition.ROLE_INFRASTRUCTURE);
+			String cacheableAttributeSourceBeanName = parserContext.getReaderContext().registerWithGeneratedName(cacheableAttributeSource);
+			RuntimeBeanReference cacheableAttributeSourceRuntimeReference = new RuntimeBeanReference(cacheableAttributeSourceBeanName);
+			
+			RootBeanDefinition cacheablePointcutSource = new RootBeanDefinition(CacheStaticMethodMatcherPointcut.class);
+			cacheablePointcutSource.setSource(elementSource);
+			cacheablePointcutSource.setRole(BeanDefinition.ROLE_INFRASTRUCTURE);
+			
+			cacheablePointcutSource.getPropertyValues().add("cacheableAttributeSource", cacheableAttributeSourceRuntimeReference);
+			String cacheablePointcutBeanName = parserContext.getReaderContext().registerWithGeneratedName(cacheablePointcutSource);
+			
+			RootBeanDefinition cachingInterceptorSource = new RootBeanDefinition(CachingInterceptor.class);
+			cachingInterceptorSource.setSource(elementSource);
+			cachingInterceptorSource.setRole(BeanDefinition.ROLE_INFRASTRUCTURE);
+			cachingInterceptorSource.getPropertyValues().add("cacheableAttributeSource", cacheableAttributeSourceRuntimeReference);
+			String cachingInterceptorBeanName = parserContext.getReaderContext().registerWithGeneratedName(cachingInterceptorSource);
+			
+			RootBeanDefinition pointcutAdvisorSource = new RootBeanDefinition(DefaultBeanFactoryPointcutAdvisor.class);
+			pointcutAdvisorSource.setSource(elementSource);
+			pointcutAdvisorSource.setRole(BeanDefinition.ROLE_INFRASTRUCTURE);
+			pointcutAdvisorSource.getPropertyValues().add("adviceBeanName", cachingInterceptorBeanName);
+			pointcutAdvisorSource.getPropertyValues().add("pointcut", cacheablePointcutBeanName);
+			
+			parserContext.getReaderContext().registerWithGeneratedName(pointcutAdvisorSource);
+		}
+		return null;
+	}
+
+}
---
+++ src/main/java/edu/wisc/services/cache/config/AnnotationDrivenEhCacheNamespaceHandler.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright 2010 The Board of Regents of the University of Wisconsin System.
+ */
+package edu.wisc.services.cache.config;
+
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
+/**
+ *
+ * @author Nicholas Blair, nblair@doit.wisc.edu
+ * @version $Id$
+ */
+public class AnnotationDrivenEhCacheNamespaceHandler extends NamespaceHandlerSupport {
+
+	/* (non-Javadoc)
+	 * @see org.springframework.beans.factory.xml.NamespaceHandler#init()
+	 */
+	@Override
+	public void init() {
+		this.registerBeanDefinitionParser("ehcache", new AnnotationDrivenEhCacheBeanDefinitionParser());
+	}
+
+}
