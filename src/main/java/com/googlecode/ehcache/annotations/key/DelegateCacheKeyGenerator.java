f141f64567ecd7b4e6587825a5eb41b61c678103
nicholas.blair@gmail.com <nicholas.blair@gmail.com@e9fffd1a-4083-750a-f54f-a748c1f33b81>
2010-04-06 15:59:23 +0000

    add new CacheKeyGenerator to simplify delegate style subclasses
    git-svn-id: http://ehcache-spring-annotations.googlecode.com/svn/trunk@210 e9fffd1a-4083-750a-f54f-a748c1f33b81

---
+++ src/main/java/com/googlecode/ehcache/annotations/key/DelegateCacheKeyGenerator.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright 2010 Nicholas Blair, Eric Dalquist
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.googlecode.ehcache.annotations.key;
+
+import java.io.Serializable;
+
+
+/**
+ * {@link AbstractCacheKeyGenerator} implementation designed for subclassing.
+ * 
+ * Subclasses can implement the {@link #getAdditionalDataForKeyGeneration()} method to
+ * seed additional data into the call to {@link #generateKey(Object...)}.
+ * 
+ * @author Nicholas Blair
+ * @version $Id$
+ */
+public abstract class DelegateCacheKeyGenerator extends AbstractCacheKeyGenerator<Serializable> {
+
+	private final AbstractCacheKeyGenerator<Serializable> delegate;
+	
+	/**
+	 * @param delegate
+	 */
+	public DelegateCacheKeyGenerator(
+			AbstractCacheKeyGenerator<Serializable> delegate) {
+		super();
+		this.delegate = delegate;
+	}
+
+	/**
+	 * Calls {@link #generateKey(Object...)} on the delegate passing in data argument AND the result
+	 * from {@link #getAdditionalDataForKeyGeneration()}.
+	 * 
+	 * @see #getAdditionalDataForKeyGeneration()
+	 * @see com.googlecode.ehcache.annotations.key.AbstractCacheKeyGenerator#generateKey(java.lang.Object[])
+	 */
+	@Override
+	public final Serializable generateKey(Object... data) {
+		this.setCheckforCycles(delegate.isCheckforCycles());
+		return this.delegate.generateKey(getAdditionalDataForKeyGeneration(), data);
+	}
+
+	/**
+	 * 
+	 * @return a {@link Serializable} object to inject into the calls to {@link #generateKey(Object...)}
+	 */
+	public abstract Serializable getAdditionalDataForKeyGeneration();
+}
