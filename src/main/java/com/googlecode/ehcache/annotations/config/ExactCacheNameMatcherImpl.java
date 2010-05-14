71b20560487679c45f4b705c82e30b9446e351a8
nicholas.blair@gmail.com <nicholas.blair@gmail.com@e9fffd1a-4083-750a-f54f-a748c1f33b81>
2010-05-14 19:39:42 +0000

    issue #11: add getters to inner field for all matchers; fix copyright header
    git-svn-id: http://ehcache-spring-annotations.googlecode.com/svn/trunk@361 e9fffd1a-4083-750a-f54f-a748c1f33b81

--- src/main/java/com/googlecode/ehcache/annotations/config/ExactCacheNameMatcherImpl.java
+++ src/main/java/com/googlecode/ehcache/annotations/config/ExactCacheNameMatcherImpl.java
@@ -1,4 +1,20 @@
 /**
+ * Copyright 2010 Nicholas Blair, Eric Dalquist
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
  * 
  */
 package com.googlecode.ehcache.annotations.config;
@@ -32,6 +48,13 @@ public class ExactCacheNameMatcherImpl implements CacheNameMatcher {
 	 */
 	public Boolean matches(String cacheName) {
 		return this.name.equals(cacheName) ? true : null;
+	}
+	
+	/**
+	 * @return the name this matcher is configured to match
+	 */
+	public String getName() {
+		return name;
 	}
 
 }
