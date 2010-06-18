1051cad7cb598ecbe38b1eaba038073544531fac
eric.dalquist@gmail.com <eric.dalquist@gmail.com@e9fffd1a-4083-750a-f54f-a748c1f33b81>
2010-06-18 19:24:40 +0000

    Little tweak to the updateSite script to ask for the local site path as well.
    git-svn-id: http://ehcache-spring-annotations.googlecode.com/svn/trunk@465 e9fffd1a-4083-750a-f54f-a748c1f33b81

--- src/etc/updateSite.sh
+++ src/etc/updateSite.sh
@@ -19,20 +19,21 @@ die () {
     exit 1
 }
 
-[ "$#" -eq 2 ] || die "2 arguments required, $# provided"
+[ "$#" -eq 3 ] || die "3 arguments required, $# provided: OLD_TAG, NEW_TAG, PATH_TO_NEW_SITE"
 
 OLD_REV=$1
 NEW_REV=$2
+NEW_SITE=$3
 
 # Copy and checkout previous site 
 svn cp -m "Creating site for $NEW_REV" https://ehcache-spring-annotations.googlecode.com/svn/site/$OLD_REV https://ehcache-spring-annotations.googlecode.com/svn/site/$NEW_REV
 svn co https://ehcache-spring-annotations.googlecode.com/svn/site/$NEW_REV
 
 # Sync the new site over the previous site, removing files that no longer exist
-rsync -avr --progress --exclude '.svn'  --delete /Users/edalquist/java/workspace/ehcache-spring-annotations_1.0-PATCHES/target/checkout/target/site/ ./$NEW_REV/
+rsync -avr --progress --exclude '.svn'  --delete $NEW_SITE/ ./$NEW_REV/
 
 # Add new files into SVN control, this will generate a lot of 'is already under version control' warnings which can be ignored
-find ./$NEW_REV/ -type f | grep -v .svn | xargs svn add
+find ./$NEW_REV/ -type f | grep -v .svn | xargs svn add --parents
 
 # Some reporting plugins generate output with inconsistent line endings, fix this
 find ./$NEW_REV/ -type f | grep -v .svn | xargs dos2unix
